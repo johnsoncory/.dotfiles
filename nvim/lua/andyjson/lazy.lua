61ec44af5e24c0a6fd326c497d51d654c520cfb8
Andy JSON <andyjson.eng@gmail.com>
2026-04-16 18:03:18 -0600

    Adding harpoon and auto/smart indenting and autopairs

--- nvim/lua/andyjson/lazy.lua
+++ nvim/lua/andyjson/lazy.lua
@@ -100,6 +100,7 @@ vim.list_extend(plugins, {
             "hrsh7th/cmp-path",
             "saadparwaiz1/cmp_luasnip",
             "L3MON4D3/LuaSnip",
+            "windwp/nvim-autopairs",
         },
         config = function()
             local cmp = require("cmp")
@@ -134,6 +135,49 @@ vim.list_extend(plugins, {
                     { name = "buffer", keyword_length = 4 },
                 }),
             })
+
+            local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+            cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+        end,
+    },
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        opts = {},
+    },
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local harpoon = require("harpoon")
+
+            harpoon:setup()
+
+            vim.keymap.set("n", "<leader>a", function()
+                harpoon:list():add()
+            end, { desc = "Harpoon add file" })
+            vim.keymap.set("n", "<leader>h", function()
+                harpoon.ui:toggle_quick_menu(harpoon:list())
+            end, { desc = "Harpoon menu" })
+            vim.keymap.set("n", "<leader>1", function()
+                harpoon:list():select(1)
+            end, { desc = "Harpoon file 1" })
+            vim.keymap.set("n", "<leader>2", function()
+                harpoon:list():select(2)
+            end, { desc = "Harpoon file 2" })
+            vim.keymap.set("n", "<leader>3", function()
+                harpoon:list():select(3)
+            end, { desc = "Harpoon file 3" })
+            vim.keymap.set("n", "<leader>4", function()
+                harpoon:list():select(4)
+            end, { desc = "Harpoon file 4" })
+            vim.keymap.set("n", "<C-p>", function()
+                harpoon:list():prev()
+            end, { desc = "Harpoon previous file" })
+            vim.keymap.set("n", "<C-n>", function()
+                harpoon:list():next()
+            end, { desc = "Harpoon next file" })
         end,
     },
     {
